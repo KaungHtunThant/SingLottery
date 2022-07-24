35ca733fcc7cd710befd0f85ef3a9e9dfe702955
eugen3199 <3199busi@gmail.com>
2022-07-24 22:52:37 +0630

    added index, static(head, header, footer), contents(dashboard), css, js, vendor

---
+++ public_html/templates/Dashboard-template.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <?php include "static/head.php" ?>
+    <body id="page-top">
+        <?php include "static/header.php" ?>
+    
+        <?php include "static/jscript.php" ?>
+    </body>
+</html>
---
+++ public_html/test.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include "static/head.php" ?>
+        <title>(Title)</title>
+    </head>
+    <body id="page-top">
+        <?php include "static/header.php" ?>
+        <?php include "contents/dashboard.php" ?>
+        <?php include "static/footer.php" ?>
+        <?php include "static/jscript.php" ?>
+    </body>
+</html>
