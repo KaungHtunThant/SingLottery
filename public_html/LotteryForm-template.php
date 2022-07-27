3998d4515bb199f180d1b02f175a07e0ffc44315
Kaung Htun Thant <53951056+eugen3199@users.noreply.github.com>
2022-07-27 17:04:02 +0630

    Edited Header.php

--- public_html/LotteryForm-template.php
+++ public_html/LotteryForm-template.php
@@ -6,7 +6,7 @@
     </head>
     <body id="page-top">
         <?php include "static/header.php" ?>
-        <?php include "contents/dashboard.php" ?>
+        <?php include "contents/lotteryform.php" ?>
         <?php include "static/footer.php" ?>
         <?php include "static/jscript.php" ?>
     </body>
--- public_html/static/header.php
+++ public_html/static/header.php
@@ -35,13 +35,17 @@
             <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
                 aria-expanded="true" aria-controls="collapseTwo">
                 <i class="fas fa-fw fa-cog"></i>
-                <span>Components</span>
+                <span>Tools</span>
             </a>
             <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                 <div class="bg-white py-2 collapse-inner rounded">
-                    <h6 class="collapse-header">Custom Components:</h6>
-                    <a class="collapse-item" href="buttons.html">Buttons</a>
-                    <a class="collapse-item" href="cards.html">Cards</a>
+                    <h6 class="collapse-header">Forms</h6>
+                    <a class="collapse-item" href="LotteryForm-template.php">Lottery Form</a>
+                    <a class="collapse-item" href="cards.html">Result Form</a>
+                    <h6 class="collapse-header">Tables</h6>
+                    <a class="collapse-item" href="buttons.html">Lottery Table</a>
+                    <a class="collapse-item" href="cards.html">Results Table</a>
+                    <a class="collapse-item" href="cards.html">Lottery Winners Table</a>
                 </div>
             </div>
         </li>
@@ -51,15 +55,15 @@
             <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
                 aria-expanded="true" aria-controls="collapseUtilities">
                 <i class="fas fa-fw fa-wrench"></i>
-                <span>Utilities</span>
+                <span>Charts</span>
             </a>
             <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
                 data-parent="#accordionSidebar">
                 <div class="bg-white py-2 collapse-inner rounded">
-                    <h6 class="collapse-header">Custom Utilities:</h6>
-                    <a class="collapse-item" href="utilities-color.html">Colors</a>
-                    <a class="collapse-item" href="utilities-border.html">Borders</a>
-                    <a class="collapse-item" href="utilities-animation.html">Animations</a>
+                    <!-- <h6 class="collapse-header">Custom Utilities:</h6> -->
+                    <a class="collapse-item" href="utilities-color.html">Earnings</a>
+                    <a class="collapse-item" href="utilities-border.html">Revenues</a>
+                    <a class="collapse-item" href="utilities-animation.html">Sales</a>
                     <a class="collapse-item" href="utilities-other.html">Other</a>
                 </div>
             </div>
