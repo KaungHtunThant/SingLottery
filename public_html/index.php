96994a864656861e7744437f56415e2629af098e
Kaung Htun Thant <53951056+eugen3199@users.noreply.github.com>
2022-07-29 21:02:13 +0630

    changes in index, templates and test.php

--- public_html/index.php
+++ public_html/index.php
@@ -6,8 +6,14 @@
     </head>
     <body id="page-top">
         <?php include "static/header.php" ?>
+
         <?php include "contents/dashboard.php" ?>
+
         <?php include "static/footer.php" ?>
-        <?php include "static/jscript.php" ?>
+        
+        <?php include "static/bootstrap_core_jscript.php" ?>
+        <?php include "static/core_plugin_jscript.php" ?>
+        <?php include "static/custom_jscript_all.php" ?>
+        <?php include "static/chart_jscript.php" ?>
     </body>
 </html>
