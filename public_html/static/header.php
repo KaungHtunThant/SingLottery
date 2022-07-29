6995c92c84f28a613289c0988db5f1117edf1728
Kaung Htun Thant <53951056+eugen3199@users.noreply.github.com>
2022-07-29 21:05:31 +0630

    Added a bunch of static php files

--- public_html/static/header.php
+++ public_html/static/header.php
@@ -9,7 +9,7 @@
             <div class="sidebar-brand-icon rotate-n-15">
                 <i class="fas fa-laugh-wink"></i>
             </div>
-            <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+            <div class="sidebar-brand-text mx-3">Sing Lottery</div>
         </a>
 
         <!-- Divider -->
@@ -17,7 +17,7 @@
 
         <!-- Nav Item - Dashboard -->
         <li class="nav-item active">
-            <a class="nav-link" href="index.html">
+            <a class="nav-link" href="index.php">
                 <i class="fas fa-fw fa-tachometer-alt"></i>
                 <span>Dashboard</span></a>
         </li>
@@ -25,10 +25,7 @@
         <!-- Divider -->
         <hr class="sidebar-divider">
 
-        <!-- Heading -->
-        <div class="sidebar-heading">
-            Interface
-        </div>
+        
 
         <!-- Nav Item - Pages Collapse Menu -->
         <li class="nav-item">
@@ -41,9 +38,10 @@
                 <div class="bg-white py-2 collapse-inner rounded">
                     <h6 class="collapse-header">Forms</h6>
                     <a class="collapse-item" href="LotteryForm-template.php">Lottery Form</a>
-                    <a class="collapse-item" href="cards.html">Result Form</a>
+                    <!-- <a class="collapse-item" href="LotteryTable-template.html">Result Form</a> -->
+                    <a class="collapse-item" href="">Result Form</a>
                     <h6 class="collapse-header">Tables</h6>
-                    <a class="collapse-item" href="buttons.html">Lottery Table</a>
+                    <a class="collapse-item" href="test.php">Lottery Table</a>
                     <a class="collapse-item" href="cards.html">Results Table</a>
                     <a class="collapse-item" href="cards.html">Lottery Winners Table</a>
                 </div>
@@ -67,49 +65,6 @@
                     <a class="collapse-item" href="utilities-other.html">Other</a>
                 </div>
             </div>
-        </li>
-
-        <!-- Divider -->
-        <hr class="sidebar-divider">
-
-        <!-- Heading -->
-        <div class="sidebar-heading">
-            Addons
-        </div>
-
-        <!-- Nav Item - Pages Collapse Menu -->
-        <li class="nav-item">
-            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
-                aria-expanded="true" aria-controls="collapsePages">
-                <i class="fas fa-fw fa-folder"></i>
-                <span>Pages</span>
-            </a>
-            <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-                <div class="bg-white py-2 collapse-inner rounded">
-                    <h6 class="collapse-header">Login Screens:</h6>
-                    <a class="collapse-item" href="login.html">Login</a>
-                    <a class="collapse-item" href="register.html">Register</a>
-                    <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-                    <div class="collapse-divider"></div>
-                    <h6 class="collapse-header">Other Pages:</h6>
-                    <a class="collapse-item" href="404.html">404 Page</a>
-                    <a class="collapse-item" href="blank.html">Blank Page</a>
-                </div>
-            </div>
-        </li>
-
-        <!-- Nav Item - Charts -->
-        <li class="nav-item">
-            <a class="nav-link" href="charts.html">
-                <i class="fas fa-fw fa-chart-area"></i>
-                <span>Charts</span></a>
-        </li>
-
-        <!-- Nav Item - Tables -->
-        <li class="nav-item">
-            <a class="nav-link" href="tables.html">
-                <i class="fas fa-fw fa-table"></i>
-                <span>Tables</span></a>
         </li>
 
         <!-- Divider -->
@@ -214,4 +169,4 @@
             </ul>
 
         </nav>
-        <!-- End of Topbar -->
+        <!-- End of Topbar-->
