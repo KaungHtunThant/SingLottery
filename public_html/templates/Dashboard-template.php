ff9bcac38da21eb810d84ceb89e6d3affb5bdca3
Kaung Htun Thant <53951056+eugen3199@users.noreply.github.com>
2022-07-29 21:13:02 +0630

    Deleted templates folder
    It complicates with linking css and js files

--- public_html/templates/Dashboard-template.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-    <?php include "static/head.php" ?>
-    <body id="page-top">
-        <?php include "static/header.php" ?>
-    
-        <?php include "static/jscript.php" ?>
-    </body>
-</html>
